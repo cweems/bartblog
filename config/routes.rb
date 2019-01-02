a7b631452e42adfd9b40928f91bdc2312c432a6f
Charlie Weems <charlie@codeforamerica.org>
2019-01-02 01:25:58 -0800

    Add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  root to: "posts#new"
+
+  authenticated do
+    root :to => 'posts#new', as: :authenticated
+  end
+
+  root to: "welcome#index"
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :posts
+
+  get '/tos', :to => 'welcome#tos', :as => :tos
+  get '/privacy', :to => 'welcome#privacy', :as => :privacy
 end
